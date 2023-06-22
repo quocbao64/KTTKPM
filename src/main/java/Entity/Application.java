2ba420fdc602bf602006af881bdbbdbcfb193426
quocbao64 <quocbao642002@gmail.com>
2023-06-22 16:44:55 +0700

    initial java-redis project

---
+++ src/main/java/Entity/Application.java
@@ -0,0 +1,36 @@
+package Entity;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPooled;
+import redis.clients.jedis.search.FTCreateParams;
+import redis.clients.jedis.search.IndexDataType;
+import redis.clients.jedis.search.Query;
+import redis.clients.jedis.search.schemafields.NumericField;
+import redis.clients.jedis.search.schemafields.TextField;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Application {
+    public static void main(String[] args) {
+        JedisPooled jedisPool = new JedisPooled("localhost", 6379);
+//        Employee employee = new Employee("Trong Beo", "Free", 1);
+//        Employee employee1 = new Employee("John", "Free", 1);
+//        Employee employee2 = new Employee("Paul", "Free", 1);
+//        jedisPool.ftCreate("idx:employee",
+//                FTCreateParams.createParams()
+//                        .on(IndexDataType.JSON)
+//                        .addPrefix("emp:"),
+//                TextField.of("$.name").as("name"),
+//                TextField.of("$.company").as("company"),
+//                NumericField.of("$.age").as("age")
+//        );
+//        jedisPool.jsonSetWithEscape("emp:1", employee);
+//        jedisPool.jsonSetWithEscape("emp:2", employee1);
+//        jedisPool.jsonSetWithEscape("emp:3", employee2);
+        var query = new Query("John");
+        var result = jedisPool.ftSearch("idx:employee", query).getDocuments();
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/Entity/Employee.java
@@ -0,0 +1,37 @@
+package Entity;
+
+public class Employee {
+    private String name;
+    private String company;
+    private int age;
+
+    public Employee(String name, String company, int age) {
+        this.name = name;
+        this.company = company;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
