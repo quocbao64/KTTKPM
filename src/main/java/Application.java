e007b3590700ba08f5d4c5808c1ef4aab956d16e
quocbao64 <quocbao642002@gmail.com>
2023-06-22 16:55:04 +0700

    refactor Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,4 @@
+import Entity.Employee;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPooled;
@@ -13,20 +14,20 @@ import java.util.Map;
 public class Application {
     public static void main(String[] args) {
         JedisPooled jedisPool = new JedisPooled("localhost", 6379);
-//        Employee employee = new Employee("Trong Beo", "Free", 1);
-//        Employee employee1 = new Employee("John", "Free", 1);
-//        Employee employee2 = new Employee("Paul", "Free", 1);
-//        jedisPool.ftCreate("idx:employee",
-//                FTCreateParams.createParams()
-//                        .on(IndexDataType.JSON)
-//                        .addPrefix("emp:"),
-//                TextField.of("$.name").as("name"),
-//                TextField.of("$.company").as("company"),
-//                NumericField.of("$.age").as("age")
-//        );
-//        jedisPool.jsonSetWithEscape("emp:1", employee);
-//        jedisPool.jsonSetWithEscape("emp:2", employee1);
-//        jedisPool.jsonSetWithEscape("emp:3", employee2);
+        Employee employee = new Employee("Trong Beo", "Free", 1);
+        Employee employee1 = new Employee("John", "Free", 1);
+        Employee employee2 = new Employee("Paul", "Free", 1);
+        jedisPool.ftCreate("idx:employee",
+                FTCreateParams.createParams()
+                        .on(IndexDataType.JSON)
+                        .addPrefix("emp:"),
+                TextField.of("$.name").as("name"),
+                TextField.of("$.company").as("company"),
+                NumericField.of("$.age").as("age")
+        );
+        jedisPool.jsonSetWithEscape("emp:1", employee);
+        jedisPool.jsonSetWithEscape("emp:2", employee1);
+        jedisPool.jsonSetWithEscape("emp:3", employee2);
         var query = new Query("John");
         var result = jedisPool.ftSearch("idx:employee", query).getDocuments();
         System.out.println(result);
